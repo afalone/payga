43e563299a4a46df194ef170b2c1c6cd41dd2fea
Evgeny Sokolov <esokolov@techranch.ru>
2016-08-03 14:46:00 +0300

    add check table existing in setup method

--- lib/payga.rb
+++ lib/payga.rb
@@ -6,6 +6,8 @@ module Payga
     end
 
     def setup which = nil
+      #TODO обратить на это внимание. Добавилено чтобы не падали миграции когда в initializers прописано уже
+      return unless ActiveRecord::Base.connection.table_exists?(System.table_name)
       cur = System.where(merchant: which).first if which
       cur ||= System.where(default: true).first
       class_variable_set('@@current', cur)
