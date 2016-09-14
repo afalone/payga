fa3958e7317f3e82d72c3ba0b24142e388ad5dfb
oleg <afa@cnwork.techranch.local>
2016-09-14 18:51:59 +0300

    sav

--- spec/models/payga/system_spec.rb
+++ spec/models/payga/system_spec.rb
@@ -2,16 +2,32 @@ require 'rails_helper'
 
 module Payga
   RSpec.describe System, type: :model do
-    subject { FactoryGirl.create(:payga_system) }
-    describe 'request' do
+    before do
+      @sys = FactoryGirl.create(:payga_system)
+    end
+    describe '#request' do
       it 'should return sber class' do
-        expect(subject.request).to eq Request::Sber
+        expect(@sys.request).to eq Request::Sber
       end
     end 
-    describe 'status' do
+    describe '#status' do
+
       # let :request, {  }
 
       # it 'should '
+    end
+
+    describe '#pay_for' do
+      before do
+        @request = FactoryGirl.create :payga_request_sber
+      end
+      it 'should setup urls' do
+        expect(@sys.request).to receive(:create).with(hash_including(amount: 1, callback_url: 'back', success_url: 'succ', fail_url: 'fail')).and_return(@request)
+        allow(@request).to receive(:pay)
+        expect(@sys.pay_for(1, '', {callback: 'back', success: 'succ', fail: 'fail'})).to eq [@request.id, @request.form_url, @request.error_code]
+      end
+
+        
     end
   end
 end
